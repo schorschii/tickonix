7c9c793965496aba925ed61634a0d6b5e2500f2b
Georg Sieber <it@georg-sieber.de>
2024-10-21 19:17:57 +0200

    class adjustment for updated css

--- foot.inc.php
+++ foot.inc.php
@@ -1,3 +1,3 @@
-<div id="vendorcontainer">
+<div id='foot'>
 	Powered by <a href='https://github.com/schorschii/tickonix' target='_blank'>Tickonix</a>, made with ❤ by <a href='https://sieber.systems' target='_blank'>Georg Sieber</a>
 </div>
