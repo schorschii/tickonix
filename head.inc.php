c14ecd8fb209d1425172cb338d2d2b1fdb890f4f
Georg Sieber <it@georg-sieber.de>
2024-11-25 21:07:26 +0100

    theme color

--- head.inc.php
+++ head.inc.php
@@ -2,6 +2,9 @@
 <meta name='viewport' content='width=device-width'>
 <link rel='stylesheet' href='css/main.css'></link>
 <link rel='icon' type='image/png' href='img/ticket.svg'>
+<?php if($db->getSetting('web-theme-color')) { ?>
+	<meta name='theme-color' content='<?php echo htmlspecialchars($db->getSetting('web-theme-color'),ENT_QUOTES); ?>'>
+<?php } ?>
 <meta name='author' content='Georg Sieber'>
 
 <style>
